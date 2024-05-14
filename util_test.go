582f51210402f2e815ab0a610b183eac67e507e6
Arik Kfir <arik@kfirs.com>
2024-05-14 21:41:16 +0300

    test: add missing tests (#2)
    This change adds a few tests.

--- util_test.go
+++ util_test.go
@@ -8,6 +8,48 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+func TestFieldNameToFlagName(t *testing.T) {
+	t.Parallel()
+	testCases := map[string]string{
+		"abc":               "abc",
+		"Abc":               "abc",
+		"ABC":               "abc",
+		"BrownFox":          "brown-fox",
+		"BrownFOX":          "brown-fox",
+		"BrownFoxJUMPSOver": "brown-fox-jumps-over",
+		"ATeam":             "a-team",
+	}
+	for fieldName, expectedFlagName := range testCases {
+		fieldName := fieldName
+		expectedFlagName := expectedFlagName
+		t.Run(fieldName, func(t *testing.T) {
+			t.Parallel()
+			With(t).Verify(fieldNameToFlagName(fieldName)).Will(EqualTo(expectedFlagName)).OrFail()
+		})
+	}
+}
+
+func TestFieldNameToEnvVarName(t *testing.T) {
+	t.Parallel()
+	testCases := map[string]string{
+		"abc":               "ABC",
+		"Abc":               "ABC",
+		"ABC":               "ABC",
+		"BrownFox":          "BROWN_FOX",
+		"BrownFOX":          "BROWN_FOX",
+		"BrownFoxJUMPSOver": "BROWN_FOX_JUMPS_OVER",
+		"ATeam":             "A_TEAM",
+	}
+	for fieldName, expectedEnvVarName := range testCases {
+		fieldName := fieldName
+		expectedFlagName := expectedEnvVarName
+		t.Run(fieldName, func(t *testing.T) {
+			t.Parallel()
+			With(t).Verify(fieldNameToEnvVarName(fieldName)).Will(EqualTo(expectedFlagName)).OrFail()
+		})
+	}
+}
+
 func Test_inferCommandFlagsAndPositionals(t *testing.T) {
 	type testCase struct {
 		root                *Command
